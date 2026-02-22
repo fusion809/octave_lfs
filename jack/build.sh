8821a157c15cf4483f6f59ccb9a1cdbd0efa7994
Brenton Horne <brentonhorne77@gmail.com>
2026-02-22 18:20:26 +1000

    Rm db5.3, impr jack

--- jack/build.sh
+++ jack/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 pkgname=jack2
 pkgver=1.9.22
-git clone https://github.com/jackaudio/jack2
+if ! [[ -d jack2 ]]; then
+	git clone https://github.com/jackaudio/jack2
+fi
+
 cd $pkgname
+git pull origin master
 sed -i -e "s|python|python3|g" waf
 ./waf configure \
   --prefix=/usr \
