8762c95b9c27d8e1287836930047048422d25cbc
Brenton Horne <brentonhorne77@gmail.com>
2026-02-22 23:23:32 +1000

    rapidjson: weaking version string and explaining reason for git ver

--- rapidjson/build.sh
+++ rapidjson/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# This builds from the latest git commit as the latest tagged release, 1.1.0,
+# is too old for GNU Octave to be able to use its version of prettywriter.
 pkgname=rapidjson
-pkgver=1.1.0
 if ! which wget &> /dev/null; then
 	echo "wget not found and used for downloading sources"
 	exit
@@ -23,6 +24,7 @@ fi
 git clone https://github.com/Tencent/rapidjson
 #wget -c https://github.com/Tencent/rapidjson/commit/3b2441b8.patch
 cd $pkgname
+pkgver=$(git log | head -n 1 | cut -d ' ' -f 2)
 find -name CMakeLists.txt | xargs sed -e 's|-Werror||' -i # Don't use -Werror
   #patch -p1 -i ../3b2441b8.patch # Fix build with GCC 14
 mkdir -p build
