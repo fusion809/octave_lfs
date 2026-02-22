1e10200c2c6628affd0b820b1c90d44bb9985a7b
Brenton Horne <brentonhorne77@gmail.com>
2026-02-23 00:50:23 +1000

    Improving versioning

--- jack/build.sh
+++ jack/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Latest tagged version won't build
 pkgname=jack2
-pkgver=1.9.22
 if ! [[ -d jack2 ]]; then
 	git clone https://github.com/jackaudio/jack2
 fi
 
 cd $pkgname
+pkgver=$(git log | head -n 1 | cut -d ' ' -f 2)
 git pull origin master
 sed -i -e "s|python|python3|g" waf
 ./waf configure \
